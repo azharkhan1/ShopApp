2819421adcef5c951f98fc29064c4aa86aa8ec13
azhar khan <azhar40@live.co.uk>
2021-02-25 10:25:20 +0500

    done deployed on heroku

--- Web/src/containers/userdashboard.jsx
+++ Web/src/containers/userdashboard.jsx
@@ -159,7 +159,7 @@ export default function UserDashboard() {
         <div>
             <div className="wrapper">
                 <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                    <a className="navbar-brand" href="#">Navbar w/ text</a>
+                    <a className="navbar-brand" href="#">{globalState.user.userEmail}</a>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon" />
                     </button>
