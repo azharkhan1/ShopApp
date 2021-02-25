dba895b8c50d030d52a247d31d9edad1e37e70f3
iamfarooqi <attaurrahmanfarooqi9@gmail.com>
2021-02-25 16:42:29 +0500

    heroku link

--- Web/src/core/index.js
+++ Web/src/core/index.js
@@ -1,6 +1,6 @@
 
-// var url = "https://shopappnavtc.herokuapp.com"
-var url = "http://localhost:5000"
+var url = "https://shopappnavtc.herokuapp.com"
+// var url = "http://localhost:5000"
 
 
 export default url;
