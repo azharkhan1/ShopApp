52d59bd954f99b40d3a75715463b62b4c5cf4b68
azhar khan <azhar40@live.co.uk>
2021-02-25 20:25:07 +0500

    fixed heorku link

--- Web/src/core/index.js
+++ Web/src/core/index.js
@@ -2,7 +2,7 @@ var url = window.location.href.split(":");
 
 if (url[0]==="https")
 {
-    url = "https://shopappnavtc.herokuapp.com/"
+    url = "https://shopappnavtc.herokuapp.com"
 }
 else{
     url = "http://localhost:5000"
